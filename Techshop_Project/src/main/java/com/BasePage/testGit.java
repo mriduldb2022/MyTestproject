cf88a3bad935d9b3d6dbac7e332caafbb1deb2cc
mridulmac2019 <mridulmac2019@localhost>
2022-10-02 23:02:11 -0400

    testing 7

--- Techshop_Project/src/main/java/com/BasePage/testGit.java
+++ Techshop_Project/src/main/java/com/BasePage/testGit.java
@@ -3,7 +3,7 @@ package com.BasePage;
 public class testGit {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("hi and hellow both test case ");
 	}
 
 }
