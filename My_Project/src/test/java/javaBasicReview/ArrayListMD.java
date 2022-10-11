4c44bf18a956b67364c23985b281628ceb474a70
mridulmac2019 <mridulmac2019@localhost>
2022-10-10 23:19:03 -0400

    Added My_project to hub

---
+++ My_Project/src/test/java/javaBasicReview/ArrayListMD.java
@@ -0,0 +1,21 @@
+package javaBasicReview;
+
+import java.util.ArrayList;
+
+public class ArrayListMD {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Beamer");
+		cars.add("Honda");
+		cars.add("mazda");
+		
+		int size = cars.size();
+		System.out.println("The size of the list : " + size); 
+		
+		
+		
+	}
+
+}
